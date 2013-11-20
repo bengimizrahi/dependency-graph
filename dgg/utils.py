5d19644da4419010b588517f918b9c43445c76d2
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2013-11-20 16:49:57 +0200

    Implement missing removeWhitespacesInDictValues()

--- dgg/utils.py
+++ dgg/utils.py
@@ -3,3 +3,8 @@ import re
 def removeWhitespaces(text):
     return re.sub(r'\s+', ' ', text).strip()
 
+def removeWhitespacesInDictValues(data):
+    newData = dict()
+    for key in data:
+        newData[key] = removeWhitespaces(data[key])
+    return newData
